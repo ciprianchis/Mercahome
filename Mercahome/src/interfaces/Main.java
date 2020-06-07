fc548abbfdc946c9ed93ef95c7d04ba46f6655e8
Ciprian Chis <ciprianchisalin@gmail.com>
2020-06-07 21:06:53 +0200

    LogInListo Falla al guardarDatos

--- Mercahome/src/interfaces/Main.java
+++ Mercahome/src/interfaces/Main.java
@@ -7,33 +7,49 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
+
+import clases.Empleado;
+import ioDatos.IoDatos;
+
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
 	private JLabel lblNewLogin;
 	private JLabel lblNewLogIn;
-	private JPanel panel;
+	private JPanel panelLog;
 	private JLabel lblNombre;
 	private JLabel lblPasswd;
-	private JTextField textFieldNombre;
+	private JTextField textFieldUserLog;
 	private JPasswordField passwordField;
 	private JButton btnEntrar;
-	private JPanel panel_1;
+	private JPanel panelSign;
 	private JLabel lblNewsIGNIN;
 	private JLabel lblNombre_1;
 	private JLabel lblPasswd_1;
-	private JTextField textField;
+	private JTextField textFieldUserSign;
 	private JButton btnNuevoUser;
-	private JTextField textField_1;
-	private JButton btnNewButton;
+	private JTextField textFieldPassSign;
+	private JButton btnNewButtonASign;
+	private ArrayList<Empleado> vEmpleados;
+	private JButton btnNewButtonALog;
+	private JLabel lblNewLabelFondo;
 
 	/**
 	 * Launch the application.
@@ -68,77 +84,264 @@ public class Main extends JFrame {
 		lblNewLogin.setBounds(10, 26, 414, 14);
 		contentPane.add(lblNewLogin);
 		
-		panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBounds(10, 51, 182, 199);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		panelLog = new JPanel();
+		panelLog.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelLog.setBounds(10, 51, 182, 199);
+		contentPane.add(panelLog);
+		panelLog.setLayout(null);
 		
 		lblNewLogIn = new JLabel("LOG IN");
 		lblNewLogIn.setBounds(71, 5, 49, 19);
 		lblNewLogIn.setFont(new Font("Sylfaen", Font.PLAIN, 14));
-		panel.add(lblNewLogIn);
+		panelLog.add(lblNewLogIn);
 		
 		lblNombre = new JLabel("Nombre:");
 		lblNombre.setBounds(10, 45, 70, 14);
-		panel.add(lblNombre);
+		panelLog.add(lblNombre);
 		
 		lblPasswd = new JLabel("Contrase\u00F1a:");
 		lblPasswd.setBounds(10, 80, 70, 14);
-		panel.add(lblPasswd);
+		panelLog.add(lblPasswd);
 		
-		textFieldNombre = new JTextField();
-		textFieldNombre.setHorizontalAlignment(SwingConstants.CENTER);
-		textFieldNombre.setText("USUARIO");
-		textFieldNombre.setBounds(90, 42, 86, 20);
-		panel.add(textFieldNombre);
-		textFieldNombre.setColumns(10);
+		textFieldUserLog = new JTextField();
+		textFieldUserLog.addFocusListener(new TextFieldUserLogFocusListener());
+		textFieldUserLog.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldUserLog.setText("USUARIO");
+		textFieldUserLog.setBounds(90, 42, 86, 20);
+		panelLog.add(textFieldUserLog);
+		textFieldUserLog.setColumns(10);
 		
 		passwordField = new JPasswordField();
+		passwordField.addFocusListener(new PasswordFieldFocusListener());
+		passwordField.addMouseListener(new PasswordFieldMouseListener());
+		passwordField.setHorizontalAlignment(SwingConstants.CENTER);
 		passwordField.setBounds(90, 77, 86, 20);
-		panel.add(passwordField);
+		panelLog.add(passwordField);
 		
 		btnEntrar = new JButton("ENTRAR");
+		btnEntrar.addMouseListener(new BtnEntrarMouseListener());
 		btnEntrar.setBounds(56, 142, 89, 23);
-		panel.add(btnEntrar);
+		panelLog.add(btnEntrar);
 		
-		panel_1 = new JPanel();
-		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_1.setLayout(null);
-		panel_1.setBounds(243, 51, 181, 199);
-		contentPane.add(panel_1);
+		panelSign = new JPanel();
+		panelSign.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelSign.setLayout(null);
+		panelSign.setBounds(243, 51, 181, 199);
+		contentPane.add(panelSign);
 		
 		lblNewsIGNIN = new JLabel("SIGN IN");
 		lblNewsIGNIN.setFont(new Font("Sylfaen", Font.PLAIN, 14));
 		lblNewsIGNIN.setBounds(71, 5, 63, 19);
-		panel_1.add(lblNewsIGNIN);
+		panelSign.add(lblNewsIGNIN);
 		
 		lblNombre_1 = new JLabel("Nombre:");
 		lblNombre_1.setBounds(10, 45, 70, 14);
-		panel_1.add(lblNombre_1);
+		panelSign.add(lblNombre_1);
 		
 		lblPasswd_1 = new JLabel("Contrase\u00F1a:");
 		lblPasswd_1.setBounds(10, 80, 70, 14);
-		panel_1.add(lblPasswd_1);
+		panelSign.add(lblPasswd_1);
 		
-		textField = new JTextField();
-		textField.setText("USUARIO");
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
-		textField.setColumns(10);
-		textField.setBounds(91, 42, 86, 20);
-		panel_1.add(textField);
+		textFieldUserSign = new JTextField();
+		textFieldUserSign.addFocusListener(new TextFieldUserSignFocusListener());
+		textFieldUserSign.setText("USUARIO");
+		textFieldUserSign.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldUserSign.setColumns(10);
+		textFieldUserSign.setBounds(91, 42, 86, 20);
+		panelSign.add(textFieldUserSign);
 		
 		btnNuevoUser = new JButton("CREAR USUARIO");
+		btnNuevoUser.addMouseListener(new BtnNuevoUserMouseListener());
+		btnNuevoUser.addActionListener(new BtnNuevoUserActionListener());
 		btnNuevoUser.setBounds(23, 142, 133, 23);
-		panel_1.add(btnNuevoUser);
+		panelSign.add(btnNuevoUser);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(91, 77, 86, 20);
-		panel_1.add(textField_1);
-		textField_1.setColumns(10);
+		textFieldPassSign = new JTextField();
+		textFieldPassSign.addFocusListener(new TextFieldPassSignFocusListener());
+		textFieldPassSign.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldPassSign.setBounds(91, 77, 86, 20);
+		panelSign.add(textFieldPassSign);
+		textFieldPassSign.setColumns(10);
 		
-		btnNewButton = new JButton("\u2194");
-		btnNewButton.setBounds(193, 50, 50, 23);
-		contentPane.add(btnNewButton);
+		btnNewButtonASign = new JButton("\u25BA");
+		btnNewButtonASign.addMouseListener(new BtnNewButtonMouseListener());
+		
+		btnNewButtonASign.setBounds(193, 108, 50, 23);
+		contentPane.add(btnNewButtonASign);
+		
+		
+		for (Component compo : panelSign.getComponents()) {
+			compo.setEnabled(false);
+		}
+		panelSign.setEnabled(false);
+		panelLog.setEnabled(true);
+		
+		passwordField.setText("CONTRASEÑA");
+		textFieldPassSign.setText("CONTRASEÑA");
+		
+		btnNewButtonALog = new JButton("\u25C4");
+		btnNewButtonALog.addMouseListener(new BtnNewButtonALogMouseListener());
+		btnNewButtonALog.setBounds(193, 142, 50, 23);
+		contentPane.add(btnNewButtonALog);
+		
+		lblNewLabelFondo = new JLabel("");
+		lblNewLabelFondo.setBounds(0, 0, 434, 261);
+		contentPane.add(lblNewLabelFondo);
+		lblNewLabelFondo.setFocusable(true);
+		lblNewLabelFondo.requestFocus();
+		btnNewButtonALog.setEnabled(false);
+		
+		IoDatos ioDatos = new IoDatos();
+		vEmpleados = ioDatos.cargarDatos();
+	}
+	private class BtnNuevoUserActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			IoDatos ioDatos = new IoDatos();
+			ioDatos.escribirFichero(vEmpleados,textFieldUserSign.getText(),textFieldPassSign.getText());
+		}
+	}
+
+	private class BtnNewButtonMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			if (panelSign.isEnabled()==false) {
+				for (Component compo : panelSign.getComponents()) {
+					compo.setEnabled(true);
+				}
+				for (Component compo : panelLog.getComponents()) {
+					compo.setEnabled(false);
+				}
+				panelSign.setEnabled(true);
+				btnNewButtonASign.setEnabled(false);
+				btnNewButtonALog.setEnabled(true);
+			}
+						
+		}
+	}
+	private class BtnNewButtonALogMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (panelLog.isEnabled()==true) {
+				for (Component compo : panelSign.getComponents()) {
+					compo.setEnabled(false);
+				}
+				for (Component compo : panelLog.getComponents()) {
+					compo.setEnabled(true);
+				}
+				panelLog.setEnabled(true);
+				panelSign.setEnabled(false);
+				btnNewButtonALog.setEnabled(false);
+				btnNewButtonASign.setEnabled(true);
+			}
+		}
+	}
+	private class TextFieldUserLogFocusListener extends FocusAdapter {
+		@Override
+		public void focusGained(FocusEvent arg0) {
+			lblNewLabelFondo.setFocusable(false);
+			textFieldUserLog.setText("");
+		}
+		@Override
+		public void focusLost(FocusEvent e) {
+			if (textFieldUserLog.getText().equalsIgnoreCase("")) {
+				textFieldUserLog.setText("USUARIO");
+			}
+		}
+	}
+	private class PasswordFieldMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			passwordField.setText("");
+		}
+	}
+	private class TextFieldUserSignFocusListener extends FocusAdapter {
+		@Override
+		public void focusGained(FocusEvent e) {
+			textFieldUserSign.setText("");
+		}
+		@Override
+		public void focusLost(FocusEvent e) {
+			if (textFieldUserSign.getText().equalsIgnoreCase("")) {
+				textFieldUserSign.setText("USUARIO");
+			}
+			
+		}
+	}
+	private class TextFieldPassSignFocusListener extends FocusAdapter {
+		@Override
+		public void focusGained(FocusEvent e) {
+			textFieldPassSign.setText("");
+		}
+		@Override
+		public void focusLost(FocusEvent e) {
+			if (textFieldPassSign.getText().equalsIgnoreCase("")) {
+				textFieldPassSign.setText("CONTRASEÑA");
+			}
+			
+		}
+	}
+	private class PasswordFieldFocusListener extends FocusAdapter {
+		@Override
+		public void focusGained(FocusEvent e) {
+			passwordField.setText("");
+		}
+		@Override
+		public void focusLost(FocusEvent e) {
+			if (String.copyValueOf(passwordField.getPassword()).equals("")) {
+				passwordField.setText("contraseña");
+			}
+			
+			if ((passwordField.getPassword().toString().equals("")) && (passwordField.getPassword().toString().equals("contraseña"))) {
+				passwordField.setText(passwordField.getPassword().toString());
+			}
+		}
+	}
+	private class BtnEntrarMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			IoDatos ioDatos = new IoDatos();
+			if((textFieldUserLog.getText().equals("")||passwordField.getPassword().equals(""))||(textFieldUserLog.getText().equals("USUARIO")||passwordField.getPassword().equals("CONTRASEÑA"))) { 
+				JOptionPane.showMessageDialog(null,"Valores no introducidos" );
+			}
+			ArrayList<Empleado> vEmpleados= ioDatos.cargarDatos();
+			
+			if (IoDatos.comprobarEmple(textFieldUserLog.getText(), String.valueOf(passwordField.getPassword()))) {
+				for (Empleado emple : vEmpleados) {
+					if (emple.getNombre().equals(textFieldUserLog.getText())) {
+						Acciones acion = new Acciones();
+						acion.setVisible(true);
+						dispose();
+					}
+				}
+			}else {
+				JOptionPane.showMessageDialog(null, "Los datos del usuario introducidos no coinciden o este no está registrado");
+				
+			}
+		}
+	}
+	private class BtnNuevoUserMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			IoDatos ioDatos= new IoDatos();
+			for (Empleado emple : vEmpleados) {
+				if (emple.getNombre().equals(textFieldUserSign.getText())) {
+					JOptionPane.showMessageDialog(null, "El usuario indicado ya fue introducido anteriormente");
+					textFieldUserSign.setText("USUARIO");
+					textFieldPassSign.setText("CONTRASEÑA");
+					return;
+				}
+			}
+			
+			
+			JOptionPane.showMessageDialog(null, "El nuevo usuario fue introducido");
+			
+			
+			
+			
+			ioDatos.escribirFichero(vEmpleados, textFieldUserSign.getText(), textFieldPassSign.getText());
+			textFieldUserSign.setText("USUARIO");
+			textFieldPassSign.setText("CONTRASEÑA");
+			
+		}
 	}
 }
--- Mercahome/src/ioDatos/IoDatos.java
+++ Mercahome/src/ioDatos/IoDatos.java
@@ -2,13 +2,56 @@ package ioDatos;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 import clases.Empleado;
 
 public class IoDatos {
+
+	private final static String url = "jdbc:mysql://localhost:3306/mercahome";
+	private final static String usuario = "root";
+	private final static String passwd = "root";
+	private static Connection con;
+
+	public void BDConecxion() {
+		con = null;
+	}
+	
+	public static void conectarBD() {
+		if (con == null) {
+			try {
+				con = DriverManager.getConnection(url, usuario, passwd);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Conectado");
+		}
+
+	}
+
+	public static void desconectarBD() {
+		if (con != null) {
+			try {
+				con.close();
+				con = null;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 
 	public ArrayList<Empleado> cargarDatos() {
 		ArrayList<Empleado> vEmpleados = new ArrayList();
@@ -41,8 +84,7 @@ public class IoDatos {
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			e.printStackTrace();		}
 
 		
 		
@@ -50,6 +92,61 @@ public class IoDatos {
 		return vEmpleados;
 	}
 	
+	
+	public void escribirFichero(ArrayList<Empleado> vEmpleados, String nombre, String pass) {
+		FileWriter fw = null;
+		PrintWriter pw = null;
+		File empleados = new File("empleados.txt");
+
+		if (!empleados.exists()) {
+			try {
+				empleados.createNewFile();
+			} catch (IOException e) {
+				System.out.println("Error al crear el fichero");
+			}
+		} else {
+
+
+			try {
+				fw = new FileWriter(empleados,true);
+				pw = new PrintWriter(fw);
+				
+				Empleado empleado = new Empleado(nombre, pass);
+				
+				for (Empleado emple: vEmpleados) {
+					pw.println(empleado.getNombre() + "-" + emple.getPasswd());
+					break;
+				}
+				
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("ERROR AL GUARDAR EL FICHERO");
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				pw.close();
+			}
+
+		}
+	}
+	
+	public static boolean comprobarEmple(String nombre, String passwd) {
+		boolean correcto = false;
+		IoDatos ioDatos = new IoDatos();
+
+		ArrayList<Empleado> vEmpleados = ioDatos.cargarDatos();
+		
+		for (Empleado emple : vEmpleados) {
+			if (emple.getNombre().equalsIgnoreCase(nombre) && emple.getPasswd().equalsIgnoreCase(passwd)) {
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
 	
 	private Empleado obtenerEmpleado(String linea) {
 		String nombre ="";
